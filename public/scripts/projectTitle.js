8d15b5f49801dcd00d8efdb7c51d83fef58982a5
Badr Ijaz <badrkmc@gmail.com>
2020-12-19 00:02:28 +0500

    hovering over projects now changes wave outline color as well

--- public/scripts/projectTitle.js
+++ public/scripts/projectTitle.js
@@ -1,22 +1,24 @@
 function projectTitle(project) {
-
     var projectTitle = document.getElementsByClassName("project-title")[0];
+    var waves = document.getElementsByClassName("wave-front-path")[0];
 
-    if (project === "azure")
-        projectTitle.style.color = "rgb(76, 201, 223)";
 
-    if (project === "locket")
+    if (project === "locket") {
         projectTitle.style.color = "#fdfd02";
+        waves.style.fill = "#fdfd02";
+    }
 
-    if (project === "steve")
+    if (project === "steve") {
         projectTitle.style.color = "#ff7e15";
+        waves.style.fill = "#ff7e15";
+    }
 
-    setTimeout(function () {
-        projectTitle.style.color = "rgb(76, 201, 223)";
-    }, 3000);
 }
 
 function resetProjectTitle() {
     var projectTitle = document.getElementsByClassName("project-title")[0];
+    var waves = document.getElementsByClassName("wave-front-path")[0];
+
     projectTitle.style.color = "rgb(76, 201, 223)";
+    waves.style.fill = "rgb(76, 201, 223)";
 }
