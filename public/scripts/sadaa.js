3ab95d18a5c3dc6fb3886b6d6ab9728eecc5a021
Badr <badrkmc@gmail.com>
2023-03-01 15:50:04 +0500

    user styling changes - collapse category styling

--- public/scripts/sadaa.js
+++ public/scripts/sadaa.js
@@ -7,14 +7,29 @@ let joinedVc = false;
 
 const categoryVcInnerHTML = categoryVC.innerHTML;
 const userInVc = `<div class="vc-users">
-  <img src="./images/github.svg" alt="avatar" />
+  <img src="./images/favicon.ico" alt="avatar" />
   <p>Badr</p>
 </div>
 `
 
+const collapsedCateogry = `<img src="./images/speaker.svg" alt="speaker" />
+  <img style="margin-left: 0.5rem;" src="./images/favicon.ico" alt="avatar" />
+`
+const collapsedCategoryDiv = document.createElement("div");
+collapsedCategoryDiv.classList.add("discord-container-vc");
+collapsedCategoryDiv.innerHTML = collapsedCateogry;
+
 categoryContainer.addEventListener("click", (event) => {
   categoryArrow.classList.toggle("rotate");
   categoryVC.classList.toggle("hide");
+  myVC.classList.toggle("hide");
+
+  if (myVC.classList.contains("hide") && joinedVc) {
+    categoryContainer.appendChild(collapsedCategoryDiv);
+    return;
+  }
+
+  categoryContainer.removeChild(collapsedCategoryDiv);
 });
 
 categoryVC.addEventListener("click", (event) => {
